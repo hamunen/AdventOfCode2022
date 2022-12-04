6b06ee436faebb6d03009675dfeaf6b5f34ce2f3
Henri Hamunen <henri.hamunen@sievo.com>
2022-12-04 11:22:52 +0200

    Day 4 solved

---
+++ src/AdventOfCode/Day04.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode;
+
+[ExcludeFromCodeCoverage]
+public class Day04 : BaseDay
+{
+    private readonly IEnumerable<SectionAssignmentPair> _sectionAssignmentPairs;
+
+    public Day04()
+    {
+        var input = File.ReadAllText(InputFilePath);
+        var lines = input.Split(Environment.NewLine);
+        _sectionAssignmentPairs =
+            lines.Select(l => SectionAssignmentPair.ParseSectionsFrom(l));
+    }
+
+    public override ValueTask<string> Solve_1() {
+        var countOfPairsWhereSectionsFullyOverlap
+            = _sectionAssignmentPairs.Count(p => p.OneSectionFullyContainsOther());
+        return new (countOfPairsWhereSectionsFullyOverlap.ToString());
+    }
+
+    public override ValueTask<string> Solve_2() {
+        var countOfOverlappingPairs =
+            _sectionAssignmentPairs.Count(p => p.RangesOverlap());
+        return new (countOfOverlappingPairs.ToString());
+    }
+}
+
+public class SectionAssignment
+{
+    public int MinSection { get; set; }
+    public int MaxSection { get; set; }
+
+    public static SectionAssignment ParseSectionFrom(string input)
+    {
+        var minAndMaxSections = input.Split('-');
+        return new SectionAssignment
+        {
+            MinSection = Int32.Parse(minAndMaxSections[0]),
+            MaxSection = Int32.Parse(minAndMaxSections[1])
+        };
+    }
+
+    public Boolean FullyContains(SectionAssignment other) =>
+        MinSection <= other.MinSection && MaxSection >= other.MaxSection;
+
+    public Boolean OverlapsWith(SectionAssignment other) => 
+        Math.Max(MinSection, other.MinSection) <= 
+        Math.Min(MaxSection, other.MaxSection);
+}
+
+public class SectionAssignmentPair
+{
+    public SectionAssignment FirstSection { get; set; }
+    public SectionAssignment SecondSection { get; set; }
+
+    public static SectionAssignmentPair ParseSectionsFrom(string inputLine)
+    {
+        var inputPair = inputLine.Split(',');
+        return new SectionAssignmentPair
+        {
+            FirstSection = SectionAssignment.ParseSectionFrom(inputPair[0]),
+            SecondSection = SectionAssignment.ParseSectionFrom(inputPair[1])
+        };
+    }
+
+    public Boolean OneSectionFullyContainsOther() => 
+        FirstSection.FullyContains(SecondSection) 
+        || SecondSection.FullyContains(FirstSection);
+
+    public Boolean RangesOverlap() => FirstSection.OverlapsWith(SecondSection);
+}
---
+++ tests/AdventOfCode.Tests/Day04Tests.cs
@@ -0,0 +1,91 @@
+﻿namespace AdventOfCode.Tests
+{
+    public class Day04Tests
+    {
+        [Theory]
+        [InlineData("2-4,6-8")]
+        [InlineData("50-55,41-48")]
+        public void ParseSectionsFrom_ParsesInputAndReturnsPair(string inputLine)
+        {
+            var result = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+            Assert.IsType<SectionAssignmentPair>(result);
+        }
+
+        [Theory]
+        [InlineData("2-4,6-8")]
+        [InlineData("50-55,41-48")]
+        [InlineData("1-2,3-4")]
+        public void OneSectionFullyContainsOther_FullyNonOverlappingPair_ReturnsFalse(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.OneSectionFullyContainsOther();
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("5-7,7-8")]
+        [InlineData("2-6,4-8")]
+        [InlineData("1-2,2-3")]
+        public void OneSectionFullyContainsOther_PartiallyOverlappingPair_ReturnsFalse(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.OneSectionFullyContainsOther();
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("2-8,3-7")]
+        [InlineData("3-7,2-8")]
+        [InlineData("6-6,4-6")]
+        [InlineData("100-200,101-199")]
+        [InlineData("100-200,100-200")]
+        public void OneSectionFullyContainsOther_FullyOverlappingPair_ReturnsTrue(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.OneSectionFullyContainsOther();
+            Assert.True(result);
+        }
+
+
+        [Theory]
+        [InlineData("2-4,6-8")]
+        [InlineData("50-55,41-48")]
+        [InlineData("1-2,3-4")]
+        public void RangesOverlap_FullyNonOverlappingPair_ReturnsFalse(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.RangesOverlap();
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("5-7,7-8")]
+        [InlineData("2-6,4-8")]
+        [InlineData("1-2,2-3")]
+        public void RangesOverlap_PartiallyOverlappingPair_ReturnsTrue(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.RangesOverlap();
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("2-8,3-7")]
+        [InlineData("3-7,2-8")]
+        [InlineData("6-6,4-6")]
+        [InlineData("100-200,101-199")]
+        [InlineData("100-200,100-200")]
+        public void RangesOverlap_FullyOverlappingPair_ReturnsTrue(string inputLine)
+        {
+            var pair = SectionAssignmentPair.ParseSectionsFrom(inputLine);
+
+            var result = pair.RangesOverlap();
+            Assert.True(result);
+        }
+    }
+}
