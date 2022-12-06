59693bad6b001c2c4979262b793ec4247a5652a3
Henri Hamunen <henri.hamunen@sievo.com>
2022-12-06 12:01:26 +0200

    Day 6 solved

---
+++ src/AdventOfCode/Day06.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode;
+
+[ExcludeFromCodeCoverage]
+public class Day06 : BaseDay
+{
+    private string _input;
+    public Day06()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var dataStream = new ElfDataStream(_input);
+        var result = dataStream.GetStartOfPacketMarkerPosition();
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var dataStream = new ElfDataStream(_input);
+        var result = dataStream.GetStartOfMessageMarkerPosition();
+        return new(result.ToString());
+    }
+}
+
+public class ElfDataStream
+{
+    private const int START_OF_PACKET_SEQUENCE_LENGTH = 4;
+    private const int START_OF_MESSAGE_SEQUENCE_LENGTH = 14;
+
+    public string Buffer { get; }
+    public ElfDataStream(string buffer)
+    {
+        Buffer = buffer;
+    }
+
+    public int GetStartOfPacketMarkerPosition() 
+        => GetUniqueCharacterSequenceEndPositionFromBuffer(
+            START_OF_PACKET_SEQUENCE_LENGTH);
+
+    public int GetStartOfMessageMarkerPosition() 
+        => GetUniqueCharacterSequenceEndPositionFromBuffer(
+            START_OF_MESSAGE_SEQUENCE_LENGTH);
+
+    private int GetUniqueCharacterSequenceEndPositionFromBuffer(
+        int sequenceLength)
+    {
+        var endIndex = sequenceLength - 1;
+        int startIndex;
+        string currentSequence;
+        while (endIndex < Buffer.Length)
+        {
+            startIndex = endIndex - sequenceLength + 1;
+            currentSequence = Buffer.Substring(startIndex, sequenceLength);
+
+            if (CharactersAreUnique(currentSequence, sequenceLength)) break;
+            endIndex++;
+        }
+
+        return endIndex + 1;
+    }
+
+    private static Boolean CharactersAreUnique(string sequence, int sequenceLength)
+    {
+        return sequence.Distinct().Count() == sequenceLength;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day06Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Tests
+{
+    public class Day06Tests
+    {
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void GetStartOfPacketMarkerPosition(string input, int expected)
+        {
+            var dataStream = new ElfDataStream(input);
+
+            var result = dataStream.GetStartOfPacketMarkerPosition();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void GetStartOfMessageMarkerPosition(string input, int expected)
+        {
+            var dataStream = new ElfDataStream(input);
+
+            var result = dataStream.GetStartOfMessageMarkerPosition();
+            Assert.Equal(expected, result);
+        }
+    }
+}
