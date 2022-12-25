a3142b121cbd96f7927d72147a6266bf8de8facc
Henri Hamunen <henri.hamunen@sievo.com>
2022-12-25 17:08:53 +0200

    Day 20 part 1

---
+++ src/AdventOfCode/Day20.cs
@@ -0,0 +1,223 @@
+﻿
+using System.Collections.Generic;
+
+namespace AdventOfCode;
+
+[ExcludeFromCodeCoverage]
+public class Day20 : BaseDay
+{
+    private readonly string _input;
+
+    public Day20()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var file = new EncryptedFile(_input);
+
+        var myList = file.MixFile();
+        Console.Clear();
+        var othList = AltSol
+            .Day20(_input.Split(Environment.NewLine).ToList());
+
+
+        var result = file.FindGroveCoordinates();
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(2.ToString());
+    }
+}
+
+public sealed class EncryptedFile
+{
+    public int[] Original { get; set; }
+    public EncryptedFile(string input)
+    {
+        Original = input.Split(Environment.NewLine).Select(line => int.Parse(line)).ToArray();
+    }
+
+    public int FindGroveCoordinates()
+    {
+        var mixed = MixFile();
+        var max = mixed.Max();
+        var min = mixed.Min();
+
+        var result = FindNthNumberAfterZero(mixed, 1000)
+            + FindNthNumberAfterZero(mixed, 2000)
+            + FindNthNumberAfterZero(mixed, 3000);
+
+        return result;
+    }
+
+    public int[] MixFile()
+    {
+        var list = Original.Select(x => (value: x, visited: false)).ToList(); // 2nd vlaue = visited
+        int i = 0;
+        while (i < Original.Length)
+        {
+            if (!list[i].visited) MoveValue(list, i);
+            if (list[i].visited) i++;
+        }
+
+        return list.Select(l => l.value).ToArray();
+    }
+
+    public static void MoveValue(List<(int value, bool visited)> list, int index)
+    {
+        Console.Write($"index: ");
+        var value = list[index].value;
+        Console.Write($"value {value}");
+
+        if (value == 0) { list[index] = (0, true); return; }
+        var newIndex = index + value; // -1 for negative?
+
+        list.RemoveAt(index);
+
+
+        if (newIndex >= list.Count) newIndex %=list.Count;
+        else if (newIndex <= 0)
+            newIndex = list.Count + (newIndex % list.Count);
+        
+        //Console.Write($", goes between {list[newIndex].value}");
+        //Console.WriteLine($" and {list[newIndex + 1 == 5000 ? 0 : newIndex + 1].value}");
+
+        list.Insert(newIndex, (value, true));
+    }
+
+    public static int FindNthNumberAfterZero(int[] numbers, int n)
+    {
+        var zeroIndex = Array.IndexOf(numbers, 0);
+        var newIndex = (zeroIndex + n) % numbers.Length;
+        return numbers[newIndex];
+    }
+}
+
+
+public sealed class AltSol
+{
+    public static int[] Day20(List<string> input)
+    {
+        var list = new LinkedList<int>();
+        var list2 = new LinkedList<long>();
+        var nodes = new List<LinkedListNode<int>>();
+        var nodes2 = new List<LinkedListNode<long>>();
+
+        const long key = 811589153;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            nodes.Add(list.AddLast(int.Parse(input[i])));
+            nodes2.Add(list2.AddLast(long.Parse(input[i]) * key));
+        }
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            var node = nodes[i];
+            int moves = node.Value;
+            Console.Write($"index: value {moves}");
+
+            if (moves < 0)
+            {
+                LinkedListNode<int> swap;
+                for (int j = 0; j < -moves; j++)
+                {
+                    swap = node.Previous ?? node.List.Last;
+                    list.Remove(node);
+                    list.AddBefore(swap, node);
+                }
+            }
+            else if (moves > 0)
+            {
+                for (int j = 0; j < moves; j++)
+                {
+                    var swap = node.Next ?? node.List.First;
+                    list.Remove(node);
+                    list.AddAfter(swap, node);
+                }
+            }
+
+            Console.Write($", goes between {node.Previous.Value}");
+            var andval = node.Next == null ? node.List.First.Value : node.Next.Value;
+            Console.WriteLine($" and {andval}");
+
+        }
+
+        return nodes.Select(n => n.Value).ToArray();
+
+        var search = nodes.First(node => node.Value == 0);
+        int nums = 0;
+        for (int i = 0; i <= 3000; i++)
+        {
+            if (i == 1000)
+            {
+                nums += search.Value;
+            }
+            else if (i == 2000)
+            {
+                nums += search.Value;
+            }
+            else if (i == 3000)
+            {
+                nums += search.Value;
+                break;
+            }
+            search = search.Next ?? search.List.First;
+        }
+
+        Console.WriteLine($"Part 1: {nums}");
+
+        for (int r = 0; r < 10; r++)
+        {
+            for (int i = 0; i < nodes2.Count; i++)
+            {
+                var node = nodes2[i];
+                long moves = node.Value % (list2.Count - 1);
+                if (moves < 0)
+                {
+                    for (int j = 0; j < -moves; j++)
+                    {
+                        var swap = node.Previous ?? node.List.Last;
+                        list2.Remove(node);
+                        list2.AddBefore(swap, node);
+                    }
+                }
+                else if (moves > 0)
+                {
+                    for (int j = 0; j < moves; j++)
+                    {
+                        var swap = node.Next ?? node.List.First;
+                        list2.Remove(node);
+                        list2.AddAfter(swap, node);
+                    }
+                }
+            }
+        }
+
+        var search2 = nodes2.First(node => node.Value == 0);
+        long nums2 = 0;
+        for (int i = 0; i <= 3000; i++)
+        {
+            if (i == 1000)
+            {
+                nums2 += search2.Value;
+            }
+            else if (i == 2000)
+            {
+                nums2 += search2.Value;
+            }
+            else if (i == 3000)
+            {
+                nums2 += search2.Value;
+                break;
+            }
+            search2 = search2.Next ?? search2.List.First;
+        }
+
+        Console.WriteLine($"Part 2: {nums2}");
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day20Tests.cs
@@ -0,0 +1,248 @@
+﻿using AoCHelper;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day20Tests
+    {
+        private readonly EncryptedFile _testFile;
+
+        public Day20Tests()
+        {
+            //var path = Path.Combine("TestInputs", "Day11_1.txt");
+            //_testInput1 = File.ReadAllText(path);
+
+            _testFile = new EncryptedFile("1\r\n2\r\n-3\r\n3\r\n-2\r\n0\r\n4");
+
+        }
+
+        [Fact]
+        public void MoveValues_MovesRight()
+        {
+            var list = new List<(int, bool)>
+            {
+                (1, true), (2, false), (3, false)
+                , (4, false), (5, false)
+            };
+
+            EncryptedFile.MoveValue(list, 1);
+            Assert.Equal(2, list[3].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeft()
+        {
+            var list = new List<(int, bool)>
+            {
+                (1, true), (2, true), (3, true), (-2, false), (5, false)
+            };
+
+            EncryptedFile.MoveValue(list, 3);
+            Assert.Equal(-2, list[1].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeft_OtherPositionsAreCorrect()
+        {
+            var list = new List<(int, bool)>
+            {
+                (1, true), (2, true), (3, true), (-2, false), (5, false)
+            };
+
+            EncryptedFile.MoveValue(list, 3);
+            Assert.Equal(3, list[3].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesRightOverTheEnd_WrapsAround()
+        {
+            var list = new List<(int, bool)>
+            {
+                (1, true), (2, false), (3, false), (4, false)
+                , (5, false)
+            };
+
+            EncryptedFile.MoveValue(list, 3);
+            Assert.Equal(4, list[3].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesRightOverTheEnd_WrapsAround2()
+        {
+            var list = new List<(int, bool)>
+            {
+            (1, false), (2, false), (-3, false), (3, false)
+            , (-2, false), (0, false), (4, false)            
+            };
+
+            EncryptedFile.MoveValue(list, 6);
+            Assert.Equal(4, list[4].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesRightToLastPos_WrapsAroundToStart()
+        {
+            var list = new List<(int, bool)>
+            {
+            (1, false), (2, false), (-3, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            EncryptedFile.MoveValue(list, 3);
+            Assert.Equal(3, list[0].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesRightByLenMinus1()
+        {
+            // 10 length
+            var list = new int[] { 1, 2, -3, 3, -2, 0, 9, 8, 2, -6 }
+                .Select(i => (i, false)).ToList();
+
+            EncryptedFile.MoveValue(list, 6);
+            Assert.Equal(9, list[6].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeftByLenMinus1()
+        {
+            // 10 length
+            var list = new int[] { 1, 2, -3, 3, -2, 0, -9, 8, 2, -6 }
+                .Select(i => (i, false)).ToList();
+
+            EncryptedFile.MoveValue(list, 6);
+            Assert.Equal(-9, list[6].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeftOverTheEnd_WrapsAround()
+        {
+            var list = new List<(int, bool)>
+            {
+            (1, false), (2, false), (-3, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            EncryptedFile.MoveValue(list, 2);
+            Assert.Equal(-3, list[5].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeftToZeroPos_WrapsAroundToEnd()
+        {
+            var list = new List<(int, bool)>
+            {
+            (1, false), (2, false), (-2, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            EncryptedFile.MoveValue(list, 2);
+            Assert.Equal(-2, list[6].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeftFromZeroPos()
+        {
+            var list = new List<(int, bool)>
+            {
+            (-3, false), (2, false), (-2, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            EncryptedFile.MoveValue(list, 0);
+            Assert.Equal(-3, list[3].Item1);
+        }
+
+        [Fact]
+        public void MoveValues_MovesLeftWithMoreThanLength()
+        {
+            // len 7
+            var list = new List<(int, bool)>
+            {
+            (2, false), (-10, false), (-2, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            // after 3
+            EncryptedFile.MoveValue(list, 1);
+            Assert.Equal(-10, list[3].Item1);
+        }
+
+
+        [Fact]
+        public void MoveValues_MovesLeftFromZeroPosWithMoreThanLength()
+        {
+            // len 7
+            var list = new List<(int, bool)>
+            {
+            (-10, false), (2, false), (-2, false), (3, false)
+            , (-2, false), (0, false), (4, false)
+            };
+
+            // after -2
+            EncryptedFile.MoveValue(list, 0);
+            Assert.Equal(-10, list[2].Item1);
+        }
+
+
+        [Fact]
+        public void MixFile()
+        {
+            var result = _testFile.MixFile();
+            var resultString = string.Join(", ", result);
+            var expected = "1, 2, -3, 4, 0, 3, -2";
+            Assert.Equal(expected, resultString);
+        }
+
+        [Theory]
+        [InlineData(1000, 4)]
+        [InlineData(2000, -3)]
+        [InlineData(3000, 2)]
+        public void FindNthnumberAfterZer_Finds1000thNumber(int n, int expected)
+        {
+            var numbers = new int[] { 1, 2, -3, 4, 0, 3, -2 };
+            var result = EncryptedFile.FindNthNumberAfterZero(numbers, n);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void MixFile_2()
+        {
+            var list = new int[] { 3, 2, 1, 0 };
+            var str = string.Join("\r\n", list);
+
+            var file = new EncryptedFile(str);
+            var result = file.MixFile();
+            var resultString = string.Join(", ", result);
+            var expected = "1, 2, 3, 0";
+            Assert.Equal(expected, resultString);
+        }
+
+        [Fact]
+        public void MixFile_3()
+        {
+            var list = new int[] { 1, 2, 3, 0 };
+            var str = string.Join("\r\n", list);
+
+            var file = new EncryptedFile(str);
+            var result = file.MixFile();
+            var resultString = string.Join(", ", result);
+            var expected = "1, 3, 2, 0";
+            Assert.Equal(expected, resultString);
+        }
+
+        [Fact]
+        public void MixFile_4()
+        {
+            // 8 length
+            var list = new int[] { 1, 1, 1, 1, 1, 0, 1, 1 };
+            var str = string.Join("\r\n", list);
+
+            var file = new EncryptedFile(str);
+            var result = file.MixFile();
+            var resultString = string.Join(", ", result);
+            var expected = "1, 1, 1, 1, 1, 1, 0, 1";
+            Assert.Equal(expected, resultString);
+        }
+    }
+}
