2fc3e649e9c2b34b0a9f07a3c196212999149d50
Henri Hamunen <henri.hamunen@sievo.com>
2022-12-29 22:19:30 +0200

    Day 24 solved, very slow

--- src/AdventOfCode/Day24.cs
+++ src/AdventOfCode/Day24.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text;
+
 namespace AdventOfCode;
 
 [ExcludeFromCodeCoverage]
@@ -14,56 +16,155 @@ public class Day24 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         var basin = new BlizzardBasin(_input);
-        basin.Play();
-        return new(2.ToString());
+        var result = basin.GetShortestPathStartToTarget();
+        return new(result.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new(2.ToString());
+        var basin = new BlizzardBasin(_input);
+        var result = basin.GetShortestPathStartToTargetAndBackAndBack();
+        return new(result.ToString());
     }
 }
+
+// This is very slow. 
 
 public sealed class BlizzardBasin
 {
     public (int x, int y) StartingPosition { get; set; }
-    public (int x, int y) Target { get; set; }
-    public int[,] UpBlizzards { get; set; }
-    public int[,] DownBlizzards { get; set; }
-    public int[,] LeftBlizzards { get; set; }
-    public int[,] RigthBlizzards { get; set; }
+    public static (int x, int y) Target { get; set; }
 
-    // byte = 8 bits...
-    public byte[,] Blizzards { get; set; }
-    public BlizzardCell[,] BlizzardCells { get; set; }
+    public BlizzardCell[,] StartingBlizzards { get; set; }
     private int _maxY;
     private int _maxX;
-    private BlizzardCell[] _movementFlags = new BlizzardCell[4] { BlizzardCell.Up, BlizzardCell.Down, BlizzardCell.Left, BlizzardCell.Right };
+    private readonly BlizzardCell[] _movementFlags = new BlizzardCell[4] { BlizzardCell.Up, BlizzardCell.Down, BlizzardCell.Left, BlizzardCell.Right };
+    private readonly BlizzardCell[][,] _blizzardPositions;
+    private readonly int _blizzardRotation;
 
     public BlizzardBasin(string input)
     {
-        StartingPosition = (1, -1);
+        StartingPosition = (0, -1);
         ParseInput(input);
+        _blizzardPositions = PopulateBlizzardPositions();
+        _blizzardRotation = _blizzardPositions.Length;
     }
 
-    public void Play()
+    public static bool SequenceEquals<TBlizzardCell>(TBlizzardCell[,] a, TBlizzardCell[,] b) => 
+        a.Cast<TBlizzardCell>().SequenceEqual(b.Cast<TBlizzardCell>());
+
+    public int GetShortestPathStartToTarget() => ShortestPath(StartingPosition, Target, 0);
+
+    public int GetShortestPathStartToTargetAndBackAndBack()
     {
-        BlizzardCells = AdvanceMinute();
+        var minutes1 = ShortestPath(StartingPosition, Target, 0);
+        var minutes2 = ShortestPath(Target, StartingPosition, minutes1);
+        var minutes3 = ShortestPath(StartingPosition, Target, minutes2);
+
+        return minutes3;
     }
 
-    private BlizzardCell[,] AdvanceMinute()
+    public int ShortestPath((int x, int y) startPos, (int x, int y) targetPos, int startMinute)
+    {
+        var start = new Position(startPos, startMinute, targetPos);
+        var best = int.MaxValue;
+
+        var visited = new List<Position>();
+
+        var unvisited = new PriorityQueue<Position, int>();
+        unvisited.Enqueue(start, start.DistanceToTarget * 100 + start.Minute);
+
+        while (unvisited.Count != 0)
+        {
+
+            var dfsPos = unvisited.Dequeue();
+            var pos = dfsPos.Pos;
+            var minute = dfsPos.Minute;
+
+            if (minute + dfsPos.DistanceToTarget >= best) continue;
+
+            if (visited.Any(v => v.Pos.x == pos.x && v.Pos.y == pos.y && v.Minute % _blizzardRotation == minute % _blizzardRotation)) 
+                // so if we have been here in the same blizzard position
+                continue;
+
+            visited.Add(dfsPos);
+
+            if (pos.y == targetPos.y && pos.x == targetPos.x)
+            {
+                best = minute;
+                continue;
+            }
+
+            var blizzardsNextMinute = _blizzardPositions[(minute + 1) % _blizzardRotation];
+            var possibleMoves = GetPossibleMovements(pos.x, pos.y, blizzardsNextMinute);
+            foreach (var newPos in possibleMoves.Select(move => new Position(move, minute + 1, targetPos)))
+            {
+                unvisited.Enqueue(newPos, newPos.DistanceToTarget * 100 + newPos.Minute );
+            }
+        }
+        return best;
+    }
+
+    private struct Position
+    {
+        public (int x, int y) Pos { get; set; }
+        public int DistanceToTarget { get; }
+        public int Minute { get; set; }
+        public Position((int x, int y) pos, int minute, (int x, int y) target) { 
+            Pos = pos; 
+            Minute = minute;
+            DistanceToTarget = Math.Abs(target.x - pos.x) + Math.Abs(target.y - pos.y);
+        }
+
+    }
+
+    private List<(int x, int y)> GetPossibleMovements(int x, int y, BlizzardCell[,] blizzards)
+    {
+        var list = new List<(int x, int y)>();
+        if (y == -1) // starting pos - down or wait are only options
+        {
+            list.Add((x, y)); // Wait
+            if (blizzards[x, y + 1] == BlizzardCell.None) list.Add((x, y + 1)); // Down
+            return list;
+        }
+        // one below starting pos - can move up
+        if (x == 0 && y == 0) list.Add((0, -1));
+
+        // target pos - up or wait
+        if (y == Target.y && x == Target.x)
+        {
+            list.Add((x, y)); // Wait
+            if (blizzards[x, y - 1] == BlizzardCell.None) list.Add((x, y - 1)); // Up
+            return list;
+        }
+        if (y == Target.y - 1 && x == Target.x)
+        {
+            list.Add((x, y + 1)); // Down
+        }
+
+        if (y < _maxY && blizzards[x, y + 1] == BlizzardCell.None) list.Add((x, y + 1)); // Down
+        if (blizzards[x, y] == BlizzardCell.None) list.Add((x, y)); // Wait
+        if (y > 0 && blizzards[x, y - 1] == BlizzardCell.None) list.Add((x, y - 1)); // Up
+        if (x > 0 && blizzards[x - 1, y] == BlizzardCell.None) list.Add((x - 1, y)); // Left
+        if (x < _maxX && blizzards[x + 1, y] == BlizzardCell.None) list.Add((x + 1, y)); // Right
+
+        // empty list = no movements, you're dead
+        return list;
+    }
+
+    private BlizzardCell[,] AdvanceMinute(BlizzardCell[,] current)
     {
         var newPositions = new BlizzardCell[_maxX + 1, _maxY + 1];
 
-        for (int y = 0; y <= _maxY; y++)
+        for (var y = 0; y <= _maxY; y++)
         {
-            for (int x = 0; x <= _maxX; x++)
+            for (var x = 0; x <= _maxX; x++)
             {
-                var cell = BlizzardCells[x, y];
-                foreach (BlizzardCell flag in _movementFlags.Where(f => cell.HasFlag(f)))
+                var cell = current[x, y];
+                foreach (var flag in _movementFlags.Where(f => cell.HasFlag(f)))
                 {
                     var newPos = GetNewPos(x, y, flag); 
-                    newPositions[newPos.x, newPos.y] |= flag; // does this work?
+                    newPositions[newPos.x, newPos.y] |= flag;
                 }
             }
         }
@@ -71,25 +172,30 @@ public sealed class BlizzardBasin
         return newPositions;
     }
 
-    private (int x, int y) GetNewPos(int x, int y, BlizzardCell cell)
-    {
-        switch (cell)
+    private (int x, int y) GetNewPos(int x, int y, BlizzardCell cell) =>  
+        cell switch
         {
-            case BlizzardCell.Up:
-                if (y == 0) return (x, _maxY);
-                return (x, y - 1);
-            case BlizzardCell.Down:
-                if (y == _maxY) return (x, 0);
-                return (x, y + 1);
-            case BlizzardCell.Left:
-                if (x == 0) return (_maxX, y);
-                return (x - 1, y);
-            case BlizzardCell.Right:
-                if (x == _maxX) return (0, y);
-                return (x + 1, y);
-            default:
-                throw new Exception();
+            BlizzardCell.Up => y == 0 ? (x, _maxY) : (x, y - 1),
+            BlizzardCell.Down => y == _maxY ? (x, 0) : (x, y + 1),
+            BlizzardCell.Left => x == 0 ? (_maxX, y) : (x - 1, y),
+            BlizzardCell.Right => x == _maxX ? (0, y) : (x + 1, y),
+            _ => throw new Exception()
+        };
+
+    public BlizzardCell[][,] PopulateBlizzardPositions()
+    {
+        var blizzardPositions = new List<BlizzardCell[,]>();
+        var current = StartingBlizzards;
+        while (true)
+        {
+            blizzardPositions.Add(current);
+
+            current = AdvanceMinute(current);
+            if (blizzardPositions.Any(p => SequenceEquals(p, current)))
+                break;
         }
+
+        return blizzardPositions.ToArray();
     }
 
     private void ParseInput(string input)
@@ -99,13 +205,13 @@ public sealed class BlizzardBasin
         _maxY = lines.Length - 3;
 
         Target = (lines[^1].IndexOf('.') - 1, _maxY + 1);
-        BlizzardCells = new BlizzardCell[_maxX + 1, _maxY + 1];
+        StartingBlizzards = new BlizzardCell[_maxX + 1, _maxY + 1];
 
         for (int y = 0; y <= _maxY; y++)
         {
             for (int x = 0; x <= _maxX; x++)
             {
-                BlizzardCells[x,y] = lines[y+1][x+1] switch
+                StartingBlizzards[x,y] = lines[y+1][x+1] switch
                 {
                     '>' => BlizzardCell.Right,
                     'v' => BlizzardCell.Down,
@@ -119,6 +225,45 @@ public sealed class BlizzardBasin
 
         //BlizzardCells[0, 0] = BlizzardCell.None | BlizzardCell.Left;
     }
+
+    public void Draw()
+    {
+        var builder = new StringBuilder();
+
+        for (int y = 0; y <= _maxY; y++)
+        {
+            for (int x = 0; x <= _maxX; x++)
+            {
+                var cell = StartingBlizzards[x, y];
+                var flags = _movementFlags.Where(f => cell.HasFlag(f)).ToList();
+                if (flags.Count > 1)
+                {
+                    builder.Append(flags.Count);
+                }
+                else if (flags.Count == 0)
+                {
+                    builder.Append('.');
+                }
+                else
+                {
+                    var ch = flags[0] switch
+                    {
+                        BlizzardCell.Right => '>',
+                        BlizzardCell.Down => 'v',
+                        BlizzardCell.Left => '<',
+                        BlizzardCell.Up => '^',
+                        BlizzardCell.None => '.',
+                        _ => throw new InvalidDataException()
+                    };
+                    builder.Append(ch);
+                }
+
+            }
+            builder.Append(Environment.NewLine);
+        }
+
+        Console.WriteLine(builder.ToString());
+    }
 }
 
 [Flags]
@@ -128,14 +273,5 @@ public enum BlizzardCell
     Up = 0x0001,
     Down = 0x0010,
     Left = 0x0100,
-    Right = 0x1000
-}
-
-// not needed?
-public struct BasinCoordinate
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public BasinCoordinate(int x, int y) { X = x; Y = y; }
+    Right = 0x1000,
 }
